7e4da7d6c19c18f64d74abac0a67dd41da77395b
Christopher Mark Gore <cgore@cgore.com>
2015-06-15 13:58:58 -0500

    personal clojars build.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clojure-msgpack "1.1.0"
+(defproject cgore/clojure-msgpack "1.2.0"
   :description "A lightweight Clojure implementation of the MessagePack spec."
-  :url "https://github.com/edma2/clojure-msgpack"
+  :url "https://github.com/cgore/clojure-msgpack"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.check "0.7.0"]]
   :global-vars {*warn-on-reflection* true}
   :scm {:name "git"
-        :url "https://github.com/edma2/clojure-msgpack"})
+        :url "https://github.com/cgore/clojure-msgpack"})
